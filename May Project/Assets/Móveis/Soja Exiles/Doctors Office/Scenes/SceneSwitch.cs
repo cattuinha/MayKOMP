1b8ceb62c8f5fa06cf0ec14fa08d4d6803240540
catuinha <Catty.Fernandes@hotmail.com>
2020-06-30 16:20:52 -0300

    arruma ai faz favor

---
+++ May Project/Assets/Móveis/Soja Exiles/Doctors Office/Scenes/SceneSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Structure_01");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Structure_02");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ May Project/Assets/Móveis/Soja Exiles/Doctors Office/Scripts & Animations/FanRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour
+{
+
+   public float spinSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( 0, spinSpeed, 0);
+    }
+}
