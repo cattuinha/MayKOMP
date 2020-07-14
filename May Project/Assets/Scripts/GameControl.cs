932ab5201711d0f604899b09e9260bf0c7ac31c3
catuinha <Catty.Fernandes@hotmail.com>
2020-07-14 20:32:17 -0300

    socorro bugo

---
+++ May Project/Assets/Scripts/GameControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] pictures;
+
+    [SerializeField]
+    private GameObject winText;
+
+    public static bool youWin;
+
+    //Use this for initialization
+    void Start ()
+    {
+        winText.SetActive(false);
+        youWin = false;
+
+    }
+   
+    //Update is called once per frame
+    void Update ()
+    {
+        if (pictures[0].rotation.z == 0 &&
+            pictures[1].rotation.z == 0 &&
+            pictures[2].rotation.z == 0 &&
+            pictures[3].rotation.z == 0 &&
+            pictures[4].rotation.z == 0 &&
+            pictures[6].rotation.z == 0)
+        {
+            youWin = true;
+            winText.SetActive(true);
+        }
+    }
+}
---
+++ May Project/Assets/Scripts/TouchRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        if (!GameControl.youWin)
+            transform.Rotate(0f, 0f, 90f);
+    }
+}
