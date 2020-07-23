819eff352948c9a44a86634ef1187daae08f27e7
catuinha <Catty.Fernandes@hotmail.com>
2020-07-23 17:46:11 -0300

    o inicio deu tudo errado

---
+++ May Project/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public string nomeDaCena;
+
+    public void ChangeS()
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+
+}
