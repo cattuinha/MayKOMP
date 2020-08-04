fd5e6e79dede206004c35905ae438a58e86d7bf4
juninhozeine <shinzo_ju@hotmail.com>
2020-08-03 21:27:28 -0300

    sexo anal

---
+++ May Project/Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string SoundPref = "SoundPref";
+    private int firstPlayInt;
+    public Slider soundSlider;
+    private float soundFloat;
+    public AudioSource[] soundAudio;
+
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+        if (firstPlayInt == 0)
+        {
+            soundFloat = .25f;
+            soundSlider.value = soundFloat;
+            PlayerPrefs.SetFloat(SoundPref, soundFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            soundFloat = PlayerPrefs.GetFloat(SoundPref);
+            soundSlider.value = soundFloat;
+        }
+
+
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(SoundPref, soundSlider.value);
+        Debug.Log("salvou");
+    }
+
+    void OnApplicationFocus (bool inFocus)
+    {
+        if (!inFocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    public void UpdateSound()
+    {
+        for (int i = 0; i < soundAudio.Length; i++)
+        {
+            soundAudio[i].volume = soundSlider.value;
+        }
+        
+    }
+}
---
+++ May Project/Assets/Scripts/SoundSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSettings : MonoBehaviour
+{
+
+    
+    private static readonly string SoundPref = "SoundPref";  
+    private float soundFloat;
+    public AudioSource[] soundAudio;
+
+    void Awake()
+    {
+        ContinueSettings();
+    }
+
+    private void ContinueSettings()
+    {
+        soundFloat = PlayerPrefs.GetFloat(SoundPref);
+
+
+        //soundAudio.volume = soundFloat;
+         //OU
+        for (int i = 0; i < soundAudio.Length; i++)
+        {
+            soundAudio[i].volume = soundFloat;
+        }
+
+        
+    }
+
+  
+}
