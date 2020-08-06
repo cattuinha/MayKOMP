dd74cd39cd1b476691f610b0decd3eaa0209ce71
juninhozeine <shinzo_ju@hotmail.com>
2020-08-06 16:44:50 -0300

    JUNIN FODA DEMAIS BRO MERECE MAMADA

---
+++ May Project/Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    private int sceneToContinue;
+    public GameObject[] continuar;
+     
+
+    void Start()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToContinue == 0)
+        {
+            for (int i = 0; i < continuar.Length; i++)
+            {
+                continuar[i].SetActive(false);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < continuar.Length; i++)
+            {
+                continuar[i].SetActive(true);
+            }
+        }
+    }
+
+    public void ContinueGame()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+
+        if (sceneToContinue != 0)
+            SceneManager.LoadScene(sceneToContinue);
+        else
+            return;
+    }
+}
---
+++ May Project/Assets/Scripts/VoltarProMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VoltarProMenu : MonoBehaviour
+{
+    private int currentSceneIndex;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (BGSoundScript.Instance.gameObject.GetComponent<AudioSource>())
+            {
+                BGSoundScript.Instance.gameObject.GetComponent<AudioSource>().Stop();
+            }
+            Cursor.visible = true;
+            Screen.lockCursor = false;
+            currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            PlayerPrefs.SetInt("SavedScene", currentSceneIndex);
+            SceneManager.LoadScene(0);
+        }
+    }
+}
